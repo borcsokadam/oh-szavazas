7a230c1e6d6809ae17339788786349a3fd11ca0c
Adam Borcsok <am.borcsok@gmail.com>
2024-01-10 17:49:27 +0100

    create project

---
+++ szavazas/src/main/java/com/oh/szavazas/SzavazasApplication.java
@@ -0,0 +1,13 @@
+package com.oh.szavazas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SzavazasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SzavazasApplication.class, args);
+    }
+
+}
---
+++ szavazas/src/test/java/com/oh/szavazas/SzavazasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oh.szavazas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SzavazasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
