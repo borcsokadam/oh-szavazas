bff11eb90f42bdad04e809317f542d8fc5778b70
Adam Borcsok <am.borcsok@gmail.com>
2024-01-11 03:10:35 +0100

    create SzavazasController class

---
+++ szavazas/src/main/java/com/oh/szavazas/controllers/SzavazasController.java
@@ -0,0 +1,29 @@
+package com.oh.szavazas.controllers;
+
+import com.oh.szavazas.dtos.HibaUzenetDTO;
+import com.oh.szavazas.models.Szavazas;
+import com.oh.szavazas.services.SzavazasService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/szavazasok")
+public class SzavazasController {
+    private SzavazasService szavazasService;
+
+    @Autowired
+    public SzavazasController(SzavazasService szavazasService) {
+        this.szavazasService = szavazasService;
+    }
+
+    @PostMapping(path = "/szavazas")
+    public ResponseEntity<?> saveSzavazas(@RequestBody Szavazas szavazas) {
+        try {
+            return ResponseEntity.status(HttpStatus.CREATED).body(szavazasService.save(szavazas));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new HibaUzenetDTO(e.getMessage()));
+        }
+    }
+}
