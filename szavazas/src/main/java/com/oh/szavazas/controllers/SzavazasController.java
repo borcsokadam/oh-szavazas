008400aa07c8433f6728d1fadf0bde714cafe7cc
Adam Borcsok <am.borcsok@gmail.com>
2024-01-11 14:54:53 +0100

    create getKepviseloAtlagReszvetel endpoint

--- szavazas/src/main/java/com/oh/szavazas/controllers/SzavazasController.java
+++ szavazas/src/main/java/com/oh/szavazas/controllers/SzavazasController.java
@@ -8,6 +8,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.text.SimpleDateFormat;
+
 @RestController
 @RequestMapping("/szavazasok")
 public class SzavazasController {
@@ -48,5 +50,14 @@ public class SzavazasController {
     @GetMapping(path = "/napi-szavazasok/{adottNap}")
     public ResponseEntity<?> getEredmenyek(@PathVariable String adottNap) {
         return ResponseEntity.status(HttpStatus.OK).body(szavazasService.getNapiSzavazasok(adottNap));
+    }
+
+    @GetMapping(path = "/kepviselo-reszvetel-atlag")
+    public ResponseEntity<?> getKepviseloAtlagReszvetel(@RequestParam String idoszakKezdete, @RequestParam String idoszakVege) {
+        try {
+            return ResponseEntity.status(HttpStatus.OK).body(szavazasService.getKepviseloAtlagReszvetel(idoszakKezdete, idoszakVege));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new HibaUzenetDTO(e.getMessage()));
+        }
     }
 }
